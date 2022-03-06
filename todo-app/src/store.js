3e0f8da939bb6b891e29e2831b09edec00a9953c
Badare450 <badarepkiemute2014@gmail.com>
2022-03-05 19:28:59 -0800

    Created Actions

--- todo-app/src/store.js
+++ todo-app/src/store.js
@@ -1,1 +1,7 @@
-import {createStore , combineReducers } from 'redux'
+import {createStore , combineReducers } from 'redux';
+
+const reducers = {};
+
+const rootReducer = combineReducers(reducers);
+
+export const configureStore = () => createStore(rootReducer);
